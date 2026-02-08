c56f48a04578cd79793b2dbe3640c3dd7dea9001
anuraag696 <anuraag.gupta@s.amity.edu>
2026-02-08 14:11:59 +0530

    Fix router for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import QuestionPage from "./pages/QuestionPage";
 import ApologyPage from "./pages/ApologyPage";
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<WelcomePage />} />
           <Route path="/questions/:id" element={<QuestionPage />} />
@@ -24,7 +24,7 @@ const App = () => (
           <Route path="/valentine" element={<ValentinePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
